248d52c6d6b594df7b9ef2b3fbe37fc415675df8
shimaaa_saleh <123778598+ShimaaSaleh2002@users.noreply.github.com>
2024-07-16 18:42:46 +0300

    splash screen completed

---
+++ lib/home/home.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:islami_app/home/tabs/ahadeeth.dart';
+import 'package:islami_app/home/tabs/quran.dart';
+import 'package:islami_app/home/tabs/radio.dart';
+import 'package:islami_app/home/tabs/sebha.dart';
+import 'package:islami_app/home/tabs/settings.dart';
+
+class HomeScreen extends StatefulWidget {
+  static const routeName = 'home';
+  HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int selectedIndex = 0;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Image.asset('assets/images/bg3.png'),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            title: const Text(
+              'إسلامي',
+              style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 30,
+                  fontFamily: 'el-messiri'),
+            ),
+            centerTitle: true,
+          ),
+          body: tabs[selectedIndex],
+          bottomNavigationBar: BottomNavigationBar(
+            items: const [
+              BottomNavigationBarItem(
+                icon: ImageIcon(AssetImage('assets/images/radio.png')),
+                label: '',
+                backgroundColor: Color(0xFFB7935F),
+              ),
+              BottomNavigationBarItem(
+                icon: ImageIcon(AssetImage('assets/images/sebha_blue.png')),
+                label: '',
+                backgroundColor: Color(0xFFB7935F),
+              ),
+              BottomNavigationBarItem(
+                icon: ImageIcon(AssetImage('assets/images/quran-quran-svgrepo-com.png')),
+                label: '',
+                backgroundColor: Color(0xFFB7935F),
+              ),
+              BottomNavigationBarItem(
+                icon: ImageIcon(AssetImage('assets/images/quran.png')),
+                label: '',
+                backgroundColor: Color(0xFFB7935F),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.settings),
+                label: '',
+                backgroundColor: Color(0xFFB7935F),
+              ),
+
+            ],
+            currentIndex: selectedIndex,
+            onTap: (value) {
+              selectedIndex = value;
+              setState(() {});
+            },
+            type: BottomNavigationBarType.shifting,
+            selectedItemColor: Colors.black,
+            unselectedItemColor: Colors.white,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            //backgroundColor: Color(0xFFB7935F),
+          ),
+        ),
+      ],
+    );
+  }
+   List <Widget> tabs =[
+    RadioTab(),
+    SebhaTab(),
+    AhadeethTab(),
+    QuranTab(),
+    SettingsTab()
+  ];
+}
